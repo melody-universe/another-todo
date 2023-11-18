1e8efd232ded61e7ac318506085c69dc5f05c1a6
Melody Universe <melody.universe.2021@gmail.com>
2023-11-17 22:50:00 -0800

    added vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: { coverage: { enabled: true, provider: "v8" } },
 });
